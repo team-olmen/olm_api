ea2954edacd43fe5c205ae7489cd4d2d31a3534a
Vincent <jan-vincent.wyszynski@charite.de>
2017-07-26 16:34:54 +0200

    add cfg.php to .gitignore

--- src/config/cfg.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cfg['db_host'] = 'localhost';
-$cfg['db_name'] = 'olm';
-$cfg['db_user'] = 'olm';
-$cfg['db_password'] = 'olm';
-$cfg['jwt_secret'] = 'äö!ad*+lüp"b$ehiu%"§ns';
-$cfg['jwt_lifetime'] = 86400;

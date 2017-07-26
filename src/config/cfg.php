b6a044b05d011b71495b7877d8ea140ccad46d07
Vincent <jan-vincent.wyszynski@charite.de>
2017-07-26 17:15:07 +0200

    fixed cfg.php

--- src/config/cfg.php
+++ src/config/cfg.php
@@ -1,8 +1,8 @@
 <?php
 $cfg['db_host'] = 'localhost';
-$cfg['db_prefix'] = 'olm_';
-$cfg['db_name'] = 'olm';
-$cfg['db_user'] = 'olm';
-$cfg['db_password'] = 'olm';
+$cfg['db_prefix'] = 'olm_test_';
+$cfg['db_name'] = 'db10642994-olmapi';
+$cfg['db_user'] = 'db10642994-olm';
+$cfg['db_password'] = 'DB273052olm';
 $cfg['jwt_secret'] = 'secretive_secret';
 $cfg['jwt_lifetime'] = 86400;

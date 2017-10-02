65f7cf6a5f8ba774f4b399998e67ef5be2ea44bd
Vincent <jan-vincent.wyszynski@charite.de>
2017-10-02 14:42:17 +0200

    use environment variables for db credentials remove web/.htaccess from repo

--- src/config/cfg.php
+++ src/config/cfg.php
@@ -1,8 +1,8 @@
 <?php
 $cfg['db_host'] = 'localhost';
-$cfg['db_prefix'] = 'olm_test_';
-$cfg['db_name'] = 'db10642994-olmapi';
-$cfg['db_user'] = 'db10642994-olm';
-$cfg['db_password'] = 'DB273052olm';
+$cfg['db_prefix'] = getenv('OLM_DB_PREFIX') ? getenv('OLM_DB_PREFIX') : 'olm_';
+$cfg['db_name'] = getenv('OLM_DB_NAME') ? getenv('OLM_DB_NAME') : 'olm';
+$cfg['db_user'] = getenv('OLM_DB_USER') ? getenv('OLM_DB_USER') : 'olm';
+$cfg['db_password'] = getenv('OLM_DB_PASSWORD') ? getenv('OLM_DB_PASSWORD') : 'olm';
 $cfg['jwt_secret'] = 'secretive_secret';
 $cfg['jwt_lifetime'] = 86400;
